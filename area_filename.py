0af397c4602b07dad51e773ef4e2097f4f63fe08
ABINAYA-del <122618252+ABINAYA-del@users.noreply.github.com>
2023-01-18 19:48:33 +0530

    Add files via upload

---
+++ area_filename.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+qn= input("enter filename:")
+y=qn.Split(".")
+print("etenion of the file :" +repr(y[-1]))
+
+
+
+# area
+pi=  3.14
+r=input(print("enter the rad of circle:"))
+area= pi*r*r
+print("the area of the circle :", area)
